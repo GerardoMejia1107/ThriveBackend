24fcd5be8d4381a9cabaaf2692f786aa011c5429
GerardoMejia1107 <00104923@uca.edu.sv>
2026-08-03 21:05:58 -0600

    feat: attaching JwtAuthenticationFilter to SecurityFilterChain

--- src/main/java/com/gerardo/thrive/config/security/SecurityConfig.java
+++ src/main/java/com/gerardo/thrive/config/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.gerardo.thrive.config.security;
 
+import com.gerardo.thrive.config.jwt.JwtAuthenticationFilter;
 import com.gerardo.thrive.user.services.CustomUserDetailsService;
+import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +14,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,6 +33,7 @@ public class SecurityConfig {
                         .hasRole("ADMIN")
                         .anyRequest()
                         .authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout
                         .logoutUrl("/api/security/user/test/logout")
                         .logoutSuccessHandler((request, response, authentication) -> {
