8c3f2d2f560e272ab4814d19105d110137221760
GerardoMejia1107 <00104923@uca.edu.sv>
2026-08-07 08:40:15 -0600

    chore: new entity jpa model (refres_tokens table)

---
+++ src/main/java/com/gerardo/thrive/config/jwt/RefreshTokenModel.java
@@ -0,0 +1,40 @@
+package com.gerardo.thrive.config.jwt;
+
+import com.gerardo.thrive.user.entities.UserModel;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+public class RefreshTokenModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "family_id", nullable = false)
+    private UUID familyId;
+
+    @Column(name = "token_hash", nullable = false, unique = true)
+    private String tokenHash;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserModel userId;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(name = "revoked_at")
+    private LocalDateTime revokedAt;
+}
