4fd4b66777753b2f81b58db0513da4dd255d731a
GerardoMejia1107 <00104923@uca.edu.sv>
2026-08-07 22:31:45 -0600

    chore: adding lombok builder and constructors to RefreshTokenModel

--- src/main/java/com/gerardo/thrive/auth/entities/RefreshTokenModel.java
+++ src/main/java/com/gerardo/thrive/auth/entities/RefreshTokenModel.java
@@ -2,8 +2,7 @@ package com.gerardo.thrive.auth.entities;
 
 import com.gerardo.thrive.user.entities.UserModel;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
@@ -14,6 +13,9 @@ import java.util.UUID;
 @Table(name = "refresh_tokens")
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefreshTokenModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
