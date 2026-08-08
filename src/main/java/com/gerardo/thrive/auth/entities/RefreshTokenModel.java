8fece207d8c869f9c15fab5f681db90969fb8433
GerardoMejia1107 <00104923@uca.edu.sv>
2026-08-07 22:30:53 -0600

    chore: switching RefreshTokenModel timestamps to Instant

--- src/main/java/com/gerardo/thrive/auth/entities/RefreshTokenModel.java
+++ src/main/java/com/gerardo/thrive/auth/entities/RefreshTokenModel.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -30,11 +31,11 @@ public class RefreshTokenModel {
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @Column(name = "expires_at", nullable = false)
-    private LocalDateTime expiresAt;
+    private Instant expiresAt;
 
     @Column(name = "revoked_at")
-    private LocalDateTime revokedAt;
+    private Instant revokedAt;
 }
